045d761902d5a9aecbb50909f847c3951bdd9d5a
Jacques Begin <jacques.andre.begin@gmail.com>
2017-03-23 19:34:05 +0000

    Adds isOdd exercise

---
+++ odd.js
@@ -0,0 +1,14 @@
+// function isOdd(num) {
+//   if (num % 2 === 1) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+function isOdd(num) {
+  return num % 2 === 1
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
