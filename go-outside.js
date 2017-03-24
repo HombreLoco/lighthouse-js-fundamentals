71d89225d3b4d80897a167fd14a1b6cdb65af2c2
Jacques Begin <jacques.andre.begin@gmail.com>
2017-03-24 07:15:35 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,45 @@
+var raining = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+
+var cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+
+var temperature = -40;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!")
+
+
+var isCitizen = true;
+var age = 19;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
